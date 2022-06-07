d73e6e8be5ea7c8d7f76c18a8ada09f7d81b9aa9
Juani05 <102042211+Juani05@users.noreply.github.com>
2022-06-07 10:46:03 -0300

    started juego

---
+++ Assets/Scripts/time.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class time : MonoBehaviour
+{
+    public Text txt_timeFloored;
+    public float elapsedTime;
+    public GameObject txt_ganaste;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        elapsedTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentTime = Time.time;
+        txt_timeFloored.text = Mathf.Floor(currentTime).ToString();
+        if (currentTime > 15)
+        {
+            txt_ganaste.SetActive(true);
+        }
+    }
+}
